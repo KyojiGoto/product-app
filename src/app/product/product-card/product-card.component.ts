c0091cc2f345a66aa80f38f3ca55709a0eba80f6
KyojiGoto <kyoji.gb@gmail.com>
2025-02-21 13:42:54 -0500

    Create product card component

---
+++ src/app/product/product-card/product-card.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {CurrencyPipe} from '@angular/common';
+import { Product } from '../product';
+
+/**
+ * This component displays a summary of a single product.
+ *
+ * @remarks
+ * Accepts a product object through Input() and emits a viewDetail event through Output() 
+ * when the "Learn More" button is clicked. 
+ * The parent component should handle routing through the event.
+ */
+@Component({
+  selector: 'app-product-card',
+  imports: [CurrencyPipe],
+  template: `
+    <section class="product">
+      <h2 class="product-name">{{product.name}}</h2>
+      <h3 class="product-price">{{product.price | currency }}</h3>
+      <p class="product-description">{{product.description}}</p>
+      <button class="product-button primary" (click)="onViewDetail()">Learn More</button>
+    </section>
+  `,
+  styleUrl: './product-card.component.css'
+})
+export class ProductCardComponent {
+  @Input() product!:Product;
+  @Output() viewDetail = new EventEmitter<number>();
+
+  onViewDetail() {
+    this.viewDetail.emit(this.product.id);
+  }
+}
